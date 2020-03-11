80bd3158ebddd009546232662ceebfd1ed4e641c
anoy <kuma.tola3@gmail.com>
2020-03-11 20:11:46 +0900

    Add failed test

---
+++ src/payroll_system/test/TestPayroll.java
@@ -0,0 +1,30 @@
+package payroll_system.test;
+
+
+import org.junit.jupiter.api.Test;
+import payroll_system.main.PayrollDatabase;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class TestPayroll {
+    @Test
+    public void testAddSalariedEmployee() {
+        int empId = 1;
+        AddSalariedEmployee t = new AddSalariedEmployee(empId, "Bob", "Home", 1000.00);
+        t.Execute();
+        Employee e = PayrollDatabase.GetEmployee(empId);
+        assertNotNull(e);
+        assertEquals("Bob", e.GetName());
+
+        PaymentClassification pc = e.GetClassification();
+        SalariedClassification sc = (SalariedClassification) pc;
+        assertNotNull(sc);
+        PaymentSchedule ps = e.GetSchedule();
+        MonthlySchedule ms = (MonthlySchedule) ps;
+        assertNotNull(ms);
+        PaymentMethod pm = e.GetMethod();
+        HoldMethod hm = (HoldMethod) pm;
+        assertNotNull(hm);
+    }
+}
